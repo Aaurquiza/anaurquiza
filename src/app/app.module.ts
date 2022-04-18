10c4345fa1f2b9252b4546b32d56b57fb1f3c5f2
Ana Urquiza <anitaurquiza@hotmail.com>
2022-04-18 00:33:52 -0300

    armando header
    ya colocada la imagen del programa, iconos de rrss y banner.
    boostrap ya iniciado en la app.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoargpComponent } from './components/logoargp/logoargp.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoargpComponent,
+    SocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
